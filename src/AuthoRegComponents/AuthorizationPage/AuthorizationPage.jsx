91659403ee1dce9d4862d1a02c08dcf11b1949cc
Kovdrya-V-A <KovdryaVladislav@gmail.com>
2021-04-19 21:28:20 +0300

    UpdateSocialNetwork_7

--- src/AuthoRegComponents/AuthorizationPage/AuthorizationPage.jsx
+++ src/AuthoRegComponents/AuthorizationPage/AuthorizationPage.jsx
@@ -1,4 +1,4 @@
-import s from '../AutoReg.module.css';
+import s from '../AuthoReg.module.css';
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import Header from "../../Components/Header/Header";
