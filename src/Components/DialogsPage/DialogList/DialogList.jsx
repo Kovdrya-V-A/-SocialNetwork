7ca3523a29809265fbeecb8450229c415df6692f
Kovdrya-V-A <KovdryaVladislav@gmail.com>
2021-08-12 16:33:55 +0300

    UpdateSocialNetwork_42

---
+++ src/Components/DialogsPage/DialogList/DialogList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from '../DialogsPage.module.css'
+import Dialog from "./Dialog/Dialog"
+
+
+const DialogList = (props) => {
+
+    let dialogsItems = props.dialogsData.map(d => d.isDeleted ? null :
+        <Dialog
+            onSetCurrentDialog={props.onSetCurrentDialog}
+            dialogAva={d.img} idDialog={d.idDialog}
+            chatName={d.name}
+            onDeleteDialog={props.onDeleteDialog}/>)
+
+    return (
+        <div className={s.dialogList}>
+            <h2>Список диалогов:</h2>
+            {props.dialogsData.length > 0 ? dialogsItems :
+                <p>У вас пока нет активых диалогов. Напишите кому - нибудь, и здесь появится диалог</p>}
+        </div>
+    )
+}
+export default DialogList
