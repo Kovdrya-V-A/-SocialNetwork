58e05ee914f16054aa082de6f8ab56ce37507231
Gusb <mihivanmih@gmail.com>
2021-09-21 01:50:31 +0300

    new style site authorization

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,12 +4,13 @@ import s from "./Header.module.css";
 const Header = () => {
     return (
         <header className={s.header}>
-            <div className={s.headerWRap}>
-                <div className={s.logo}><img
-                    src='https://wonder-day.com/wp-content/uploads/2020/10/wonder-day-among-us-21.png'/></div>
-                <div className={s.title}><p>VLopatiche</p></div>
+            <div className="container">
+                <div className={s.headerWRap}>
+                    <div className={s.logo}><img
+                        src='https://wonder-day.com/wp-content/uploads/2020/10/wonder-day-among-us-21.png'/></div>
+                    <div className={s.title}><p>VLopatiche</p></div>
+                </div>
             </div>
-
         </header>
     );
 }
