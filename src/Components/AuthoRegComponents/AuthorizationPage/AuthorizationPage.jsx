f045c2426166a02055345445b48f2de200bbf42a
Kovdrya-V-A <KovdryaVladislav@gmail.com>
2021-09-21 03:26:26 +0300

    SocialNetworkUpdate 62

--- src/Components/AuthoRegComponents/AuthorizationPage/AuthorizationPage.jsx
+++ src/Components/AuthoRegComponents/AuthorizationPage/AuthorizationPage.jsx
@@ -14,51 +14,45 @@ const AuthorizationPage = (props) => {
         props.onUserVerification(formData.login, formData.password)
     }
     return (
-        <>
+        <div className={s.wrapper}>
             <Header/>
-            <div className={`container ${s.authorisation_block}`}>
-                <div className={`${s.authorisation_center} p-3`}>
-                    <div className="text-center mb-3">
+            <div className={s.main}>
+                <div className={s.form}>
+                    <div className={s.authorisation}>
                         <h3>Авторизация</h3>
-                    </div>
-                    <ReduxAuthorizationForm authorisationInProgress={props.authorisationInProgress}
-                                            onSubmit={onSubmit}/>
-                    <hr/>
-                    <div className={'btn btn-success w-100'}>
-                        <NavLink className={s.link} to="/RegistrationPage">Регистрация</NavLink>
+                        <ReduxAuthorizationForm authorisationInProgress = {props.authorisationInProgress}  onSubmit = {onSubmit}/>
+                        <div className={s.regLink}>
+                            <p>Еще не зарегистрированы ? <NavLink className={s.link}
+                                                                  to="/RegistrationPage">Регистрация</NavLink></p>
+                        </div>
                     </div>
                 </div>
             </div>
             <Footer/>
-        </>
+        </div>
     );
 }
 
 const InputForm = CreateFormItem("input")
 
-//вместо div лучше использовать label тогда при клике по названию будет фокус на поле ввода
-//но лучше вообще избегать таких объяснений. От этого давно отшли. Например https://www.facebook.com/
 let AuthorizationForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
-            <div className={'mb-4'}>
-                {/* <label htmlFor="login">Введите логин:</label>*/}
-                <Field id="login" className="form-control" type="text" placeholder="Логин" name={"login"}
-                       validate={[requiredField]} component={InputForm}/>
+            <div className={s.enterLogin}>
+                <p>Введите логин:</p>
+                <Field type="text" placeholder="Логин" name ={"login"} validate={[requiredField]} component={InputForm} />
             </div>
-            <div className={'mb-4'}>
-                {/* <label htmlFor="pass">Введите пароль:</label>*/}
-                <Field id="pass" className="form-control" type="password" placeholder="Пароль" name={"password"}
-                       validate={[requiredField]} component={InputForm}/>
+            <div className={s.enterPassword}>
+                <p>Введите пароль:</p>
+                <Field type="password" placeholder="Пароль" name ={"password"} validate = {[requiredField]} component={InputForm}/>
             </div>
             <div className={s.entryButton}>
-                {props.error ? <div className={s.summaryErrorBar}>
+                {props.error ?<div className={s.summaryErrorBar}>
                     <spans className={sForm.summaryError}>
                         {props.error}
                     </spans>
-                </div> : null}
+                </div>:null}
                 <button
-                    className={'btn btn-primary ps-4 pe-4'}
                     type="submit"
                     disabled={props.authorisationInProgress}>
                     Вход
--- src/Components/Common/FormsElements/FormsElements.jsx
+++ src/Components/Common/FormsElements/FormsElements.jsx
@@ -5,13 +5,9 @@ import sForm from "./FormsElements.module.css"
 export const CreateFormItem = (Component) => ({input, meta, ...props}) => {
     const haveError = meta.touched && meta.error
 
-    //Если приходит класс className то подставляем его, если класс не приходит происходит прошлая логика
-    //Нужно что бы подлючить стили бутстрапа
     return (
         <div className={sForm.formBar}>
-            <Component {...input} {...props}
-                       className={props.className ? props.className : Component === "textarea" ? sForm.textareaForm : sForm.inputForm + " " + (haveError ? sForm.error : null)}
-            />
+            <Component {...input} {...props} className={Component === "textarea" ? sForm.textareaForm : sForm.inputForm + " " + (haveError ? sForm.error : null)}/>
             {haveError ?
                 <span className={sForm.errorText}>{meta.error}</span>
                 : null}
