9f240d15ed2072f1688e7976278bd592fd896123
Kovdrya-V-A <KovdryaVladislav@gmail.com>
2021-09-21 03:21:28 +0300

    SocialNetworkUpdate 62

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,10 +3,8 @@ import s from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <footer className={'fixed-bottom', s.footer}>
-            <div className="container">
-                © Vlad-Corporation 2020 — 2021
-            </div>
+        <footer className={s.footer}>
+            FOOTER11
         </footer>
     )
 }
