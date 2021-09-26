b94250a5538678a4bd592e4b81226390979c8f1e
Kovdrya-V-A <KovdryaVladislav@gmail.com>
2021-09-26 17:31:03 +0300

    comm

--- src/Assets/Preloaders/mainPreloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import mainPreloader from "../Preloaders/mainPreloader.svg"
-
-export const MainPreloader = () => {
-    return (
-            <img src={mainPreloader} alt="Preloader"  height="100px"
-                 width="100px"/>
-    )
-}
